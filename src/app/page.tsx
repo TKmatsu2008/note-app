34130fdef5bb1c2cabc045e7756d0bf77e01b8ea
TKmatsu2008 <fhenixeaglu1st@icloud.com>
2025-12-19 22:07:26 +0900

    chore: Supabase接続設定と初期マイグレーションの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
     <div>
       <main>
         <div className="h-[90vh] w-[90vw]">
-          <a href="">グエーーーー</a>
           <Editor />
         </div>
       </main>
